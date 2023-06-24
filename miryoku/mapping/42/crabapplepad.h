0b1c02a0365053000511c0576629d8bc8627787d
Sergei Silnov <sergei.silnov@espressif.com>
2023-06-24 21:52:21 +0200

    Fix case for russian chars

--- miryoku/mapping/42/crabapplepad.h
+++ miryoku/mapping/42/crabapplepad.h
@@ -9,8 +9,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp LBRC   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp RBRC \
-&kp TILDE  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
+&kp LBKT   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp RBKT \
+&kp GRAVE  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
 &kp CAPS   K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp BSLH \
                      K32  K33  K34       K35  K36  K37
 
